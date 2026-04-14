fa672ac86d2e99c1e66e1711af17e33a63849760
yaki-onigiri <117330897+ yaki-onigiri@users.noreply.github.com>
2026-04-14 11:48:46 +0900

    feat:カウントのマイナス制限（0未満防止）を追加

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const countEl = document.getElementById("count");
 const incrementBtn = document.getElementById("increment");
-const decrementBtn = document.getElementById("decrement")
-const resetBtn = document.getElementById("reset")
+const decrementBtn = document.getElementById("decrement");
+const resetBtn = document.getElementById("reset");
 
 let count = 0;
 
@@ -11,14 +11,15 @@ function updateDisplay() {
 
 incrementBtn.addEventListener("click", function() {
     count++;
-        // 値を変更
-
     updateDisplay();
-        // 画面更新
 });
 
 decrementBtn.addEventListener("click", function() {
-    count--;
+
+    if (count > 0) {
+        count--;
+    }
+    
     updateDisplay();
 });
 
