8f1e8746d42622b23491260afea5ef14f8a96f3d
yaki-onigiri <117330897+ yaki-onigiri@users.noreply.github.com>
2026-05-09 16:12:03 +0900

    feat: 数値変更時にアニメーションと色による視覚フィードバックを追加

--- js/script.js
+++ js/script.js
@@ -10,18 +10,25 @@ const STAGE1_INTERVAL = 250;
 const STAGE2_INTERVAL = 130;
 const STAGE3_INTERVAL = 80;
 
-
-
 let count = 0;
 let isPressing = false;
 let isLongPress = false;
 let pressStartTime = 0;
 let timerId;
 
-function updateDisplay() {
+
+// Task5-3: カウントが +1 → 緑、-1 → 赤になるように条件分岐で関数を分ける
+function updateDisplay(step) {
+
     countEl.textContent = count;
 
-    animateCount();
+    if (step > 0) {
+        animateCount("up");
+    }
+
+    if (step < 0) {
+        animateCount("down");
+    }
 }
 
 function getInterval(duration) {
@@ -37,7 +44,10 @@ function changeCount(step) {
     if (count < 0) {
         count = 0;
     }
-    updateDisplay();
+
+    // Task5-4:  step で+1/-1された数字を画面に表示するコードを追加修正
+    // updateDisplay() → updateDisplay(step)
+    updateDisplay(step);
 }
 
 function startCounting(step) {
@@ -105,8 +115,47 @@ resetBtn.addEventListener("click", function() {
     updateDisplay();
 });
 
-function animateCount() {
+// function animateCount() {
+//     countEl.classList.remove("pop");
+//     void countEl.offsetWidth;
+//     countEl.classList.add("pop");
+
+//     // Task5-2: 現在の状態がわかる視覚フィードバックを追加する
+//     countEl.classList.add("count-up");
+
+//     countEl.classList.add("flash-up");
+
+//     setTimeout(() => {
+//         countEl.classList.remove("count-up");
+    
+//         countEl.classList.remove("flash-up");
+
+//     }, 200);
+// }
+
+// 上記のコードでは、-1ボタン押下でも+1ボタン押下したように緑色のアニメーションが付くので、以下のように修正。
+
+function animateCount(type) {
+
     countEl.classList.remove("pop");
+    countEl.classList.remove("flash-up");
+    countEl.classList.remove("flash-down");
+
     void countEl.offsetWidth;
+
     countEl.classList.add("pop");
+
+    if (type === "up") {
+        countEl.classList.add("flash-up");
+    }
+
+    if (type === "down") {
+        countEl.classList.add("flash-down");
+    }
+
+    // Task5-5. +1/-1ボタンをそれぞれ押した瞬間の色・アニメーションを追加
+    setTimeout(() => {
+        countEl.classList.remove("flash-up");
+        countEl.classList.remove("flash-down");
+    }, 200);
 }
